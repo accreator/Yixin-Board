f6e251c87831cd357eeeffbc581a4efb87566227
accreator <sunkaicn@gmail.com>
2013-09-19 00:31:42 +0800

    Orignal Version

---
+++ yixin.h
@@ -0,0 +1,22 @@
+void send_command(char *command);
+void make_move(int y, int x);
+gboolean on_button_press_windowmain(GtkWidget *widget, GdkEventButton *event, GdkWindowEdge edge);
+int is_integer(const char *str);
+void show_dialog_settings(GtkWidget *widget, gpointer data);
+void show_dialog_load(GtkWidget *widget, gpointer data);
+void show_dialog_save(GtkWidget *widget, gpointer data);
+void show_dialog_about(GtkWidget *widget, gpointer data);
+void new_game(GtkWidget *widget, gpointer data);
+void change_rule(GtkWidget *widget, gpointer data);
+void change_side(GtkWidget *widget, gpointer data);
+void change_side_menu(int flag, GtkWidget *w);
+GdkPixbuf * copy_subpixbuf(GdkPixbuf *_src, int src_x, int src_y, int width, int height);
+void create_windowmain();
+gboolean iochannelout_watch(GIOChannel *channel, GIOCondition cond, gpointer data);
+gboolean iochannelerr_watch(GIOChannel *channel, GIOCondition cond, gpointer data);
+void load_setting();
+void load_engine();
+void init_engine();
+#define max(x, y) ((x)>(y)?(x):(y))
+#define min(x, y) ((x)<(y)?(x):(y))
+#define VERSION "0.2"
